5a3716d65389ee9592971445244024f2932ab9b0
mengfr <mfr452@qq.com>
2024-03-12 09:25:52 +0800

    update hosts

--- update.ps1
+++ update.ps1
@@ -1,7 +1,8 @@
-cd d:/tmp/codes/hosts;
+cd D:\tmp\codes\hosts;
 pwd;
 npm run make;
-git add .
-git commit -m "update hosts"
-git push origin master
-git push gitee master
+git add .;
+git commit -m "update hosts";
+git push origin master;
+git push gitee master;
+exit 0;
